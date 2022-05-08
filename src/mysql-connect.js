ec28ac1938f942552726507fcc3c0909b9701e5e
nicolasantonielli <nicolasantonielli@users.noreply.github.com>
2022-05-07 23:11:41 -0300

    Update mysql-connect.js

--- src/mysql-connect.js
+++ src/mysql-connect.js
@@ -11,12 +11,12 @@ var connection = mysql.createConnection({
     database: 'appTickets'
 });
 
-connection.connect();
+//connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-  console.log('The solution is: ', rows[0].solution);
-});
+//connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+**  if (err) throw err;
+//  console.log('The solution is: ', rows[0].solution);
+//});
 //connection.end();
 
 connection.connect();
