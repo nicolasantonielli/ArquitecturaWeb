cc96e15ebda775783e0c5ef92246cc3f0b3ca6fd
nicolasantonielli <nicolasantonielli@users.noreply.github.com>
2022-05-07 23:12:07 -0300

    Update mysql-connect.js

--- src/mysql-connect.js
+++ src/mysql-connect.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
 //connection.connect();
 
 //connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-**  if (err) throw err;
+//  if (err) throw err;
 //  console.log('The solution is: ', rows[0].solution);
 //});
 //connection.end();
