e9fd1933046f801326e4d41be1c3ede0168220b8
nicolasantonielli <nicolasantonielli@users.noreply.github.com>
2022-05-08 00:10:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ app.use(express.static('./css'))
 
 app.use(bodyParser.json());
 
-// all requests
-
+// log todos los requests
 app.use((req, res, next) => {
 
     console.log(`${req.method}: ${req.path} - ${moment().format(moment.HTML5_FMT.DATETIME_LOCAL_MS)}`);
@@ -98,7 +97,22 @@ app.get('/maria', async (req, res) => {
 
 app.post('/api/v1/tickets', (req, res) => {
 
+
+
+
+connection.connect();
+
+connection.query("INSERT INTO `CLIENTE`(`cuitCliente`, `nombreCliente`, `direccionCliente`) VALUES ('[value-1]','[value-2]','[value-3]')", function(err, rows, fields) {
+if (err) throw err;
+console.log('The solution is: ', rows[0].solution);
+});
+connection.end();
+
+    let ticket = req.body;
+    
+
     res.status(201).send(req.body);
+    console.log(ticket.cuitCliente);
 
 });
 
