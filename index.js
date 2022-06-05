a80b1a088f709f1db42d40332d2c1f80c7afac2b
nicolasantonielli <nicolasantonielli@users.noreply.github.com>
2022-06-05 03:52:27 -0300

    56132

--- index.js
+++ index.js
@@ -86,10 +86,11 @@ app.post('/api/v1/tickets', function (req, res) {
 
 app.get('/api/v1/tickets/:idTicket', function (req, res) {
 
-    let resultado = listarTicketById(req.params.idTicket);
+    let resultado = listarTicketById(req.params.idTicket)
 
     console.log(resultado);
-    res.body.json(resultado);
+    
+    res.json(resultado);
 });
 
 
